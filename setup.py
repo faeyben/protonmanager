87d9ba1203d7e4a4bc190664b10144d91b31334c
Fabian Niepelt <fniepelt@faeyben.de>
2022-03-21 20:28:48 +0100

    Add python 3.10 require

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
+    python_requires='>=3.10',
     install_requires=[
         'Click',
         'requests'
